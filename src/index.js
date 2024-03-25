28efe78d7a8d47abdddb19de1bf6e5079c2a6426
grethaga <163857709+grethaga@users.noreply.github.com>
2024-03-25 12:47:44 +0100

    added js to change city

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-alert("Helloo");
+function showWeather(event) {
+  event.preventDefault();
+  let cityInput = document.querySelector("#city-input");
+  let city = document.querySelector("#city");
+  city.innerHTML = cityInput.value;
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", showWeather);

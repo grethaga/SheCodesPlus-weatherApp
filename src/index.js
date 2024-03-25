4f2594929decb805f830e2dad0719f2c4bea461f
grethagall <100848664+grethagall@users.noreply.github.com>
2024-03-25 13:10:32 +0100

    added serch functionality using api

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
+function updateWeather(response) {
+  let temp = document.querySelector("#temp-value");
+  temp.innerHTML = Math.round(response.data.temperature.current);
+  let city = document.querySelector("#city");
+  city.innerHTML = response.data.city;
+}
+
+function searchCity(city) {
+  let apiKey = "o0d0713230t29ff3bf6accc034c782b3";
+  let url = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(url).then(updateWeather);
+}
+
 function showWeather(event) {
   event.preventDefault();
   let cityInput = document.querySelector("#city-input");
-  let city = document.querySelector("#city");
-  city.innerHTML = cityInput.value;
+
+  searchCity(cityInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", showWeather);
+
+searchCity("Vienna");
